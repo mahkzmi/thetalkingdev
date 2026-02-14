14e728c7a3697ac5636b103ea04febafb0c75465
Mahdieh Kazemi <mah.kzmi21@gmail.com>
2026-02-14 18:22:16 +0330

    register post in admin dashboard

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
